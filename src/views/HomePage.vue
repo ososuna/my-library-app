4cb6be249b61e8546fb138de390f63a6ecd63ff6
oswi <oswaldo.adrian@live.com.mx>
2022-10-04 17:31:13 -0500

    minor changes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,19 @@
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+</script>
 <template>
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>My Library</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">My Library</ion-title>
         </ion-toolbar>
       </ion-header>
     
@@ -20,22 +24,6 @@
     </ion-content>
   </ion-page>
 </template>
-
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HomePage',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
-});
-</script>
 
 <style scoped>
 #container {
