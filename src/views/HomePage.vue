f22bbb0c26f3ab4ca5d1d926d70f7cb6dead8533
oswi <oswaldo.adrian@live.com.mx>
2022-10-05 00:31:40 -0500

    ui changes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -53,20 +53,16 @@ loadBooks();
         <ion-title>My Library</ion-title>
       </ion-toolbar>
     </ion-header>
-
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">My Library</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <ListBookComponent
-          :books="books"
-          @onClick="openUpdateModal"
-        />
-      </div>
+      <ListBookComponent
+        :books="books"
+        @onClick="openUpdateModal"
+      />
       <ion-fab vertical="bottom" horizontal="end" slot="fixed">
         <ion-fab-button @click="openCreateModal">
           <ion-icon :icon="add"></ion-icon>
@@ -75,37 +71,3 @@ loadBooks();
     </ion-content>
   </ion-page>
 </template>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-
-ion-icon {
-  color: white;
-}
-</style>
