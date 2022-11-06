194c2f39e18297de4b272872d9f81a8c52b195d9
oswi <oswaldo.adrian@live.com.mx>
2022-11-06 15:03:39 -0600

    validate empty fields

--- src/components/auth/SignUpFormComponent.vue
+++ src/components/auth/SignUpFormComponent.vue
@@ -11,8 +11,11 @@ import {
   IonNote
 } from '@ionic/vue';
 import { validateEmail } from '@/helpers/validateEmail';
+import { useUi } from '@/hooks/useUi';
 import Role from '@/models/auth/Role';
 import SignUpForm from '@/models/ui/SignUpForm';
+
+const { setAlertMessage } = useUi();
 
 const signUpRequest = ref({
   firstName: '',
@@ -35,6 +38,8 @@ const passwordValidation = ref({
   message:   ''
 });
 
+const errorMessage = ref('');
+
 const validateEmailInput = () => {
   emailValidation.value.isValid = false;
   emailValidation.value.isInvalid = false;
@@ -42,9 +47,11 @@ const validateEmailInput = () => {
   if (validateEmail(signUpRequest.value.email)) {
     emailValidation.value.isValid = true;
     emailValidation.value.isInvalid = false;
+    errorMessage.value = '';
   } else {
     emailValidation.value.isValid = false;
     emailValidation.value.isInvalid = true;
+    errorMessage.value = 'Invalid email';
   }
 }
 
@@ -56,16 +63,25 @@ const validatePasswordInput = () => {
     passwordValidation.value.isValid = false;
     passwordValidation.value.isInvalid = true;
     passwordValidation.value.message = 'Password must be at least 6 characters long';
+    errorMessage.value = passwordValidation.value.message;
   } else if ((signUpRequest.value.password !== signUpRequest.value.passwordConfirm) && signUpRequest.value.passwordConfirm !== '') {
     passwordValidation.value.isValid = false;
     passwordValidation.value.isInvalid = true;
     passwordValidation.value.message = 'Passwords do not match';
+    errorMessage.value = passwordValidation.value.message;
   } else {
     passwordValidation.value.isValid = true;
     passwordValidation.value.isInvalid = false;
+    errorMessage.value = '';
   }
 }
 
+const onSubmit = () => {
+  if (errorMessage.value !== '') {
+    setAlertMessage(errorMessage.value);
+    return;
+  }
+}
 
 watch(signUpRequest.value, () => {
   if (signUpRequest.value.email !== '') validateEmailInput();
@@ -75,67 +91,73 @@ watch(signUpRequest.value, () => {
 </script>
 <template>
   <ion-grid>
-    <ion-row>
-      <ion-col size="12">
-        <ion-item :class="{ 'ion-valid': emailValidation.isValid, 'ion-invalid': emailValidation.isInvalid }">
-          <ion-label position="floating">Email</ion-label>
-          <ion-input
-            v-model="signUpRequest.email"
-            placeholder="Enter your email"
-            type="email"
-          >
-          </ion-input>
-          <ion-note slot="helper">Enter a valid email</ion-note>
-          <ion-note slot="error">Invalid email</ion-note>
-        </ion-item>
-      </ion-col>
-    </ion-row>
-    <ion-row>
-      <ion-col size="6">
-        <ion-item>
-          <ion-label position="floating">First name</ion-label>
-          <ion-input v-model="signUpRequest.firstName" placeholder="Enter your first name"></ion-input>
-        </ion-item>
-      </ion-col>
-      <ion-col size="6">
-        <ion-item>
-          <ion-label position="floating">Last name</ion-label>
-          <ion-input v-model="signUpRequest.lastName" placeholder="Enter your last name"></ion-input>
-        </ion-item>
-      </ion-col>
-    </ion-row>
-    <ion-row>
-      <ion-col size="12">
-        <ion-item>
-          <ion-label position="floating">Age</ion-label>
-          <ion-input v-model="signUpRequest.age" placeholder="Enter your age" type="number"></ion-input>
-        </ion-item>
-      </ion-col>
-    </ion-row>
-    <ion-row>
-      <ion-col size="12">
-        <ion-item :class="{ 'ion-valid': passwordValidation.isValid, 'ion-invalid': passwordValidation.isInvalid }">
-          <ion-label position="floating">Password</ion-label>
-          <ion-input v-model="signUpRequest.password" placeholder="Enter your password" type="password"></ion-input>
-          <ion-note slot="helper">Enter your password</ion-note>
-          <ion-note slot="error">{{ passwordValidation.message }}</ion-note>
-        </ion-item>
-      </ion-col>
-    </ion-row>
-    <ion-row>
-      <ion-col size="12">
-        <ion-item :class="{ 'ion-valid': passwordValidation.isValid, 'ion-invalid': passwordValidation.isInvalid }">
-          <ion-label position="floating">Confirm password</ion-label>
-          <ion-input v-model="signUpRequest.passwordConfirm" placeholder="Confirm your password" type="password"></ion-input>
-          <ion-note slot="helper">Confirm your password</ion-note>
-          <ion-note v-if="signUpRequest.passwordConfirm!==''" slot="error">{{ passwordValidation.message }}</ion-note>
-        </ion-item>
-      </ion-col>
-    </ion-row>
-    <ion-row class="ion-margin-top">
-      <ion-col size="12" class="center">
-        <ion-button expand="block">Sign up</ion-button>
-      </ion-col>
-    </ion-row>
+    <form @submit.prevent="onSubmit">
+      <ion-row>
+        <ion-col size="12">
+          <ion-item :class="{ 'ion-valid': emailValidation.isValid, 'ion-invalid': emailValidation.isInvalid }">
+            <ion-label position="floating">Email</ion-label>
+            <ion-input
+              required
+              v-model="signUpRequest.email"
+              placeholder="Enter your email"
+              type="email"
+            >
+            </ion-input>
+            <ion-note slot="helper">Enter a valid email</ion-note>
+            <ion-note slot="error">Invalid email</ion-note>
+          </ion-item>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col size="6">
+          <ion-item>
+            <ion-label position="floating">First name</ion-label>
+            <ion-input required v-model="signUpRequest.firstName" placeholder="Enter your first name"></ion-input>
+            <ion-note slot="error">Enter your first name</ion-note>
+          </ion-item>
+        </ion-col>
+        <ion-col size="6">
+          <ion-item>
+            <ion-label position="floating">Last name</ion-label>
+            <ion-input required v-model="signUpRequest.lastName" placeholder="Enter your last name"></ion-input>
+            <ion-note slot="error">Enter your last name</ion-note>
+          </ion-item>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col size="12">
+          <ion-item>
+            <ion-label position="floating">Age</ion-label>
+            <ion-input required v-model="signUpRequest.age" placeholder="Enter your age" type="number"></ion-input>
+            <ion-note slot="error">Enter a valid age</ion-note>
+          </ion-item>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col size="12">
+          <ion-item :class="{ 'ion-valid': passwordValidation.isValid, 'ion-invalid': passwordValidation.isInvalid }">
+            <ion-label position="floating">Password</ion-label>
+            <ion-input required v-model="signUpRequest.password" placeholder="Enter your password" type="password"></ion-input>
+            <ion-note slot="helper">Enter your password</ion-note>
+            <ion-note slot="error">{{ passwordValidation.message }}</ion-note>
+          </ion-item>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col size="12">
+          <ion-item :class="{ 'ion-valid': passwordValidation.isValid, 'ion-invalid': passwordValidation.isInvalid }">
+            <ion-label position="floating">Confirm password</ion-label>
+            <ion-input required v-model="signUpRequest.passwordConfirm" placeholder="Confirm your password" type="password"></ion-input>
+            <ion-note slot="helper">Confirm your password</ion-note>
+            <ion-note v-if="signUpRequest.passwordConfirm!==''" slot="error">{{ passwordValidation.message }}</ion-note>
+          </ion-item>
+        </ion-col>
+      </ion-row>
+      <ion-row class="ion-margin-top">
+        <ion-col size="12" class="center">
+          <ion-button type="submit" expand="block">Sign up</ion-button>
+        </ion-col>
+      </ion-row>
+    </form>
   </ion-grid>
 </template>
