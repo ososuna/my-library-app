1a5814d5d18948844522d1ebf3162e58eb0d9915
oswi <oswaldo.adrian@live.com.mx>
2022-11-09 20:02:35 -0600

    card note component

---
+++ src/components/note/CardNoteComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+<template>
+<ion-card>
+  <ion-card-header>
+    <ion-card-subtitle>
+      Tue 8 Nov 2022
+    </ion-card-subtitle>
+    <ion-card-title>Title</ion-card-title>
+  </ion-card-header>
+  <ion-card-content>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, totam minima! Aut iste et dolor dicta, unde mollitia, perferendis quasi corporis beatae aliquid laudantium tempore sint a minus eaque. Consectetur.</p>
+  </ion-card-content>
+</ion-card>
+</template>
