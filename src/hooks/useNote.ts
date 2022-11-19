3786c1d90431a53acc90c8fa1f611b85cec7ed0d
oswi <oswaldo.adrian@live.com.mx>
2022-11-19 15:12:33 -0600

    update use note hook

--- src/hooks/useNote.ts
+++ src/hooks/useNote.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import noteApi from '@/api/noteApi';
 import NewNoteDto from '@/models/dto/NewNoteDto';
+import UpdateNoteDto from '@/models/dto/UpdateNoteDto';
 
 export const useNote = () => { 
   const getNotesByBookId = async(bookId: number) => {
     try {
-      const { data } = await noteApi.get(`/${bookId}`);
+      const { data } = await noteApi.get(`/book/${bookId}`);
       return { ok: true, data };
     } catch ( error ) {
       if ( axios.isAxiosError( error ) ) {
@@ -15,6 +16,19 @@ export const useNote = () => {
         return { ok: false, message: 'An error has occurred' };
       }
     }   
+  }
+  const getNoteById = async( id: number ) => {
+    try {
+      const { data } = await noteApi.get(`/${id}`);
+      return { ok: true, data };
+    } catch ( error ) {
+      if ( axios.isAxiosError( error ) ) {
+        const { message } = error.response?.data as any || 'An error has occurred';
+        return { ok: false, message };
+      } else {
+        return { ok: false, message: 'An error has occurred' };
+      }
+    }
   }
   const createNote = async(note: NewNoteDto) => {
     try {
@@ -29,8 +43,37 @@ export const useNote = () => {
       }
     }
   }
+  const updateNote = async( id: number, note: UpdateNoteDto ) => {
+    try {
+      await noteApi.put(`/${id}`, note);
+      return { ok: true, message: 'The note has been updated &#129303' }
+    } catch ( error ) {
+      if ( axios.isAxiosError( error ) ) {
+        const { message } = error.response?.data as any || 'An error has occurred';
+        return { ok: false, message };
+      } else {
+        return { ok: false, message: 'An error has ocurred' };
+      }
+    }
+  }
+  const deleteNote = async( id: number ) => {
+    try {
+      await noteApi.delete(`/${id}`);
+      return { ok: true, message: 'The note has been deleted' }
+    } catch ( error ) {
+      if ( axios.isAxiosError( error ) ) {
+        const { message } = error.response?.data as any || 'An error has occurred';
+        return { ok: false, message };
+      } else {
+        return { ok: false, message: 'An error has ocurred' };
+      }
+    }
+  }
   return {
     getNotesByBookId,
-    createNote
+    getNoteById,
+    createNote,
+    deleteNote,
+    updateNote
   }
 }
--- src/views/note/EditNotePage.vue
+++ src/views/note/EditNotePage.vue
@@ -6,12 +6,42 @@ import {
   IonTitle,
   IonToolbar,
   IonIcon,
-  IonHeader
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonList,
+  IonTextarea
 } from '@ionic/vue';
-import { arrowBack, trash } from 'ionicons/icons';
-import { useRouter } from 'vue-router';
+import { arrowBack, save } from 'ionicons/icons';
+import { useRouter, useRoute } from 'vue-router';
+import { ref, onUpdated } from 'vue';
+import { useNote } from '@/hooks/useNote';
+import { useUi } from '@/hooks/useUi';
+import UpdateNoteDto from '@/models/dto/UpdateNoteDto';
 
+const { updateNote } = useNote();
+const route = useRoute();
 const router = useRouter();
+const { setAlertMessage } = useUi();
+
+const updateNoteForm = ref({
+  name: '',
+  description: ''
+} as UpdateNoteDto);
+
+const saveNote = async() => {
+  const { ok, message } = await updateNote(Number(route.params['id']), updateNoteForm.value);
+  setAlertMessage(message);
+  if (ok) router.go(-1);
+};
+
+onUpdated(() => {
+  updateNoteForm.value = {
+    name: '',
+    description: ''
+  };
+});
 
 </script>
 
@@ -20,20 +50,27 @@ const router = useRouter();
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-buttons slot="start">
-          <ion-icon @click="router.go(-1)" slot="icon-only" :icon="arrowBack"></ion-icon>
+          <ion-icon @click="router.push({ name: 'note', params: { bookId: route.params['bookId'] } })" slot="icon-only" :icon="arrowBack"></ion-icon>
         </ion-buttons>
         <ion-buttons slot="end">
-          <ion-icon slot="icon-only" :icon="trash" color="danger"></ion-icon>
+          <ion-icon @click="saveNote" slot="icon-only" :icon="save" color="primary"></ion-icon>
         </ion-buttons>
         <ion-title>New note</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">New note</ion-title>
-        </ion-toolbar>
-      </ion-header>
+      <ion-list class="ion-margin-top">
+        <ion-item>
+          <ion-label position="stacked">Title</ion-label>
+          <ion-input placeholder="Note's title" v-model="updateNoteForm.name"></ion-input>
+        </ion-item>
+      </ion-list>
+      <ion-list class="ion-margin-top">
+        <ion-item>
+          <ion-label position="stacked">Content</ion-label>
+          <ion-textarea :auto-grow="true" placeholder="Note's content" :rows="24" v-model="updateNoteForm.description"></ion-textarea>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
